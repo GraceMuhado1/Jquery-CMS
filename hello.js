5bf4ee5db478106c3b26c90e048978408f9b2b49
GraceMuhado1 <36431845+GraceMuhado1@users.noreply.github.com>
2018-11-21 09:22:34 +0800

    responsive column and modal

--- hello.js
+++ hello.js
@@ -1,115 +1,73 @@
 $(document).ready(function () {
 
-   
-    //  $('.action-button').click(function (e) {
-    //         e.preventDefault();
-          
-    //         $('.info').html('<h2>ok</h2> ');
-           
-            
-           
-    //         console.log("succes");
-    //         //  resultDisplay = $('.info');
-    //     });
-        //Logic goes here
-        var main = $(".main");
-        // var getResult = resultDisplay;
-    
-        $.ajax({
-            url: 'http://pure-forest-79660.herokuapp.com/footballers',
-            type: "GET",
-            dataType: 'JSON',
-    
-            success: function (response) {
-               
-                // getResult.html('result:' + response.footballers[0].imageUrl);
-    
-                if (response.success) {
-                    var footballers = response.footballers;
-                   
-                  //  var profileurl = response.imageUrl;
-                    //  resultimg.html('result:' + footballers[0].imageUrl ); href="'+footballers[i].imageUrl+'" 
-    
-                    for (var i = 0; i < footballers.length; i++) {
-    
-                        var imageUrl = footballers[i].imageUrl;
-                        var listItem =
-                            '<div class="footballer-card">' +
-                            '<div class="row">' +
-                            '<div class="col-md-3">' +
-                            '<a  class = "'+footballers[i].nationality+'" onClick="Test(\''+imageUrl+'\')"><img src=" ' + footballers[i].imageUrl + ' " class="footballer-img"></a>' +
-                            '</div>' +
-                           
-                            // ' <img src=" '+ footballers[0].imageUrl +' " class="footballer-img" id = "resultDiv" ></img>'
-                            '<div class="col-md-8">' +
-                            '<p class="footballer-details">' +
-                            '<p class="name"><b>Name: </b>' + footballers[i].name + '<p/>' +
-                            '<p class="club"><b>Club: </b>' + footballers[i].club + '<p/>' +
-                            '</p>'
-                        '</div>' +
-                            '</div>' +
-                            '<div class = "info"> </div>' +
-                            '</div>'
-                            ;
+    var main = $(".resultmain");
+    var maintwo = $(".resultwo");
+
+
+    $.ajax({
+        url: 'http://pure-forest-79660.herokuapp.com/footballers',
+        type: "GET",
+        dataType: 'JSON',
+
+        success: function (response) {
+
+
+            if (response.success) {
+                var footballers = response.footballers;
+
+
+                for (var i = 0; i < footballers.length; i++) {
+
+                    //var imageUrl = footballers[i].imageUrl;
+
+                    var slicestwo = footballers.slice(0, 3);
+
+                    var slices = footballers.slice(4, 7);
+
+                    var listItem =
+                        '<li class="nav-item col-md-2 " onclick = "pbaPop()" >' +
+                        '<a class="nav-link "  onClick="Test(\'' + slices[i].imageUrl + '\')"> <img src=" ' + slices[i].imageUrl + ' "></a>' +
+                        '</li>';
+
+                            main.append(listItem);
+
+                    var listItemtwo =
+                        '<li class="nav-item col-md-2 " onclick = "pbaPop()" >' +
+
                         
-                        main.append(listItem);
-                      
-                       //$('.'+footballers[i].nationality).click(function (footballers) {
-     
-                          //console.log('ok' +  footballers[i].name );
-                     //  console.log($this)
-                     //});
-    
-    
-                       // $('.'+i).click( createCallBack( footballers ));
-    
-                    // $('.'+footballers[i].nationality).click(function () {
-                    //    // e.preventDefault();
-                      
-                    //   //  $('.info').html('<h2>ok</h2> ');
-                       
-                    //   var footballers = response.footballers;
-                    // //    alert(footballers[i].name);
-                    //     console.log(footballers[i].name);
-                    //     //  resultDisplay = $('.info');
-                    // });  
+                        '<a class="nav-link"  onClick="Test(\'' + slicestwo[i].imageUrl + '\')"> <img src=" ' + slicestwo[i].imageUrl + ' "></a>' +
+                        '</li>';
+
+                          maintwo.append(listItemtwo);
                     }
-    
-    
-                }
-    
-                else {
-                    alert("Oops, an error occured fetching the remote data.");
-                }
             }
-    
-        });
-    
-    
-        //for ( var i = 0; i < 5; i++ ) {
-        //  $( "#clickview" ).eq( i ).on( "click", { value: i }, function( event ) {
-        // var msgs = [
-        //  "button = " + $( this ).index(),
-        //  "event.data.value = " + event.data.value,
-        //     "i = " + i
-        // ];
-        // logDiv.append( msgs.join( ", " ) + "<br>" );
-        // });
-        //  }
-    
+
+            else {
+                alert("Oops, an error occured fetching the remote data.");
+            }
+        }
+
     });
-    
-    function Test(imageUrl) {
-        console.log('ok ' + imageUrl);
-        $('.info').html('<img src = "'+imageUrl+'">');
-    }
-      
-    // function createCallBack( footballers ){
-    //     return function(){
-    //        alert('ok' + i);
-    // console.log('ok');
-    //         //console.log(footballers[i].name);
-    //     }
-    // }
-    
-    
+
+});
+
+function Test(imageUrl) {
+    console.log('ok ' + imageUrl);
+    $('.info').html('<span >x</span><img src = "' + imageUrl + '">');
+}
+
+
+document.getElementById('content').style.display="none";
+function pbaPop(){
+   
+    document.getElementById('thisinfo').style.display="block";
+    document.getElementById('content').style.display="block";
+}//end iconPop JS function
+
+function pbaUnPop(){
+    document.getElementById('thisinfo').style.display="none";
+   document.getElementById('content').style.display="none";
+}
+
+
+
